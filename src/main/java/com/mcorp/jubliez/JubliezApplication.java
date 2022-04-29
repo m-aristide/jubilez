5ec2c115f85c040109e45cc24e0cce35ac1e6a28
Aristide MASSAGA <aristidemassaga@gmail.com>
2022-04-29 05:02:38 +0100

    first commit

---
+++ src/main/java/com/mcorp/jubliez/JubliezApplication.java
@@ -0,0 +1,13 @@
+package com.mcorp.jubliez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JubliezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JubliezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcorp/jubliez/JubliezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcorp.jubliez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JubliezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
